ef61f1894db9c1da4d89ff8fa547e37d5dd2522c
Robin O'shea <oshea.robinho@gmail.com>
2022-10-18 00:46:41 +0100

    changes

--- src/BobsBurgersInventory.py
+++ src/BobsBurgersInventory.py
@@ -64,7 +64,12 @@ if __name__ == "__main__":
         _df_to_csv(df, path=config("CSV_INVENTORY_PATH"), file_name="BobsBurgersInventory")
 
     elif(len(sys.argv) >= 2):
-        df = pd.read_csv(f'{config("CSV_INVENTORY_PATH")}BobsBurgersInventory-{date.today()}.csv')
+        
+        try:
+            df = pd.read_csv(f'{config("CSV_INVENTORY_PATH")}BobsBurgersInventory-{date.today()}.csv')
+        except:
+            nory_inventory_logger.info('No Latest CSV Created, Please Run program by default first to create csv to read')
+
         func = sys.argv[1]
         user_input = (' ').join(sys.argv[2:])
 
